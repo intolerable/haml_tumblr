0f04904366313fbe6ef48a1f89071bd870d8a2b1
Fraser Murray <fraser@oxidi.se>
2012-05-14 19:00:10 +0100

    added coffeescript support

--- lib/haml_tumblr.rb
+++ lib/haml_tumblr.rb
@@ -10,30 +10,48 @@ def tab
   tab_down
 end
 
+def coffee_tag( coffee_file )
+  haml_tag :script, :type => 'text/javascript' do
+    haml_concat CoffeeScript.compile File.read "#{coffee_file.to_s}.coffee"
+  end
+end
+
 def sass_tag( sass_file )
   haml_tag :style, :type => 'text/css' do
     haml_concat Sass::Engine.new(File.read "#{sass_file.to_s}.sass").render
   end
 end
 
-def scss_tag( sass_file )
+def scss_tag( scss_file )
   haml_tag :style, :type => 'text/css' do
-    haml_concat Sass::Engine.new(File.read "#{sass_file.to_s}.scss").render
+    haml_concat Sass::Engine.new(File.read "#{scss_file.to_s}.scss").render
   end
 end
 
 def tumblr_tag( tag )
   if block_given?
-    haml_concat "{block:#{tag.to_s.capitalize}}"
+    haml_concat "{block:#{process tag}}"
     tab do
       yield
     end
-    haml_concat "{/block:#{tag.to_s.capitalize}}"
+    haml_concat "{/block:#{process tag}}"
   else
-    haml_concat "{#{tag.to_s.capitalize}}"
+    haml_concat "{#{process tag}}"
   end
 end
 
 def tumblr_link_tag( tag )
-  "{#{tag.to_s.capitalize}}"
+  "{#{process tag}}"
+end
+
+def link_to( text, address )
+  haml_tag :a, :href => address do
+    haml_concat text
+  end
+end
+
+private
+
+def process( tag )
+  tag.to_s.split("_").map(&:capitalize).join
 end
